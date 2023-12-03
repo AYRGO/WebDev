b05e76c7e1f31cb939c3861827399a0d92ab6337
kbautstaaa <149758681+kbautstaaa@users.noreply.github.com>
2023-12-03 20:05:38 +0800

    Update index.php

--- Visitor/admin/index.php
+++ Visitor/admin/index.php
@@ -2,16 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
-		<h3 class="text-primary">Admin Login page</h3>
-		<hr style="border-top:1px dotted #ccc;"/>
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
+
 			<?php if(isset($_SESSION['message'])): ?>
 				<div class="alert alert-<?php echo $_SESSION['message']['alert'] ?> msg"><?php echo $_SESSION['message']['text'] ?></div>
 			<script>
@@ -27,23 +22,29 @@
 				// clearing the message
 				unset($_SESSION['message']);
 			?>
-			<form action="function.php" method="POST">	
-				<h4 class="text-success">Don Honorio Ventura State University</h4>
-				<hr style="border-top:1px groovy #000;">
-				<div class="form-group">
-					<label>Email:</label>
-					<input type="text" class="form-control" name="Email" required />
-				</div>
-				<div class="form-group">
-					<label>Password:</label>
-					<input type="password" class="form-control" name="Password" required />
-				</div>
-				<br />
-				<div class="form-group">
-					<button class="btn btn-primary form-control" name="login">Login</button>
-				</div>
-			</form>
-		</div>
-	</div>
+
+<img src="logooo.jpg" class="dhvsu">
+    <div class="wrapper">
+    <form action="function.php" method="POST">    
+        <div style="width:500px;">
+  
+            <h2>Admin</h2>
+    </div>
+            <div class="input-box">
+            <input type="email" placeholder ="Email" name="Email" required>
+            <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+            <input type="password" placeholder ="Password" name="Password" required>
+            <i class='bx bxs-lock-alt'></i>
+            </div>
+            
+            <button type="submit" class="btn" name="login">Login </a></button>
+        
+	</form>
+    	</div>
+			
+		
+	
 </body>
 </html>
